41a82d8e02ac9ac0c4155a00720e2079c33fee02
Carlos Tovar <kharlozm@gmail.com>
2022-03-17 22:39:29 -0500

    Sesion 17 de marzo. Aprendiendo sobre Pipes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-pipes';
+
+  saludo = "HoLa muNDo";
+  numero = 20.5156;
+  pago = 1540;
+  fecha = new Date();
+
+  estudiante = {
+    nombre: "Carlos",
+    apellido: "Tovar",
+    profesion: "Ing. de Sistemas",
+    direccion: {
+      nomenclatura: "Calle 1 # 1-01",
+      barrio: "Centro",
+      ciudad: "Cali"
+    },
+    email: "carlos@tovar.com"
+  }
+
+  promesa = new Promise( (resolve) => {
+    setTimeout(() => {
+      resolve("Datos recibidos!!!");
+    }, 5000);
+  });
 }
